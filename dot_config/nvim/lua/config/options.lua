60168b46c10696cfc4fbd8065a44b44149e51564
Aletheios42 <alejandropintosdev@gmail.com>
2025-04-25 22:46:43 +0200

    Prueba definitiva

---
+++ dot_config/nvim/lua/config/options.lua
@@ -0,0 +1,38 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+
+-- no apila las lineas largas
+vim.opt.wrap = false
+
+-- convierte los espacios en tabs
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+
+vim.opt.clipboard = "unnamedplus"
+
+vim.opt.scrolloff = 999
+vim.opt.virtualedit = "block"
+
+-- modo interectivo para los comandos de sustitucioon(Pantallita ocn los cambios)
+vim.opt.inccommand = "split"
+vim.opt.ignorecase = true
+
+--  para permitir que nvim enseñe por pantalla todos los colores que permita tu terminal
+vim.opt.termguicolors = true
+
+vim.g.mapleader = " "
+
+
+-- Para que la búsqueda de archivos incluya subdirectorios
+vim.opt.path:append "**" 
+-- Muestra el comando que estás escribiendo en la parte inferior derecha
+vim.opt.showcmd = true 
+
